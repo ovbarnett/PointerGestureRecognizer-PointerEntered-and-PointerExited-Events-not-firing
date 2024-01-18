b2196808fc668f2d61e936ddb9f16130d395ee69
ovbarnett <62110195+ovbarnett@users.noreply.github.com>
2024-01-18 14:27:57 -0700

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp_PointerGestureRecognizers_Example
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiApp_PointerGestureRecognizers_Example
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        async void OnPointerEntered(object sender, PointerEventArgs e)
+        {
+            // Handle the pointer entered event
+            await DisplayAlert("OnPointerEntered Fired", "OnPointerEntered Fired","ok");
+        }
+
+        async void OnPointerExited(object sender, PointerEventArgs e)
+        {
+            // Handle the pointer exited event
+            await DisplayAlert("OnPointerExited Fired", "OnPointerExited Fired", "ok");
+        }
+
+        async void Image_Tapped(object sender, EventArgs e)
+        {
+            // Handle the pointer exited event
+            await DisplayAlert("Image_Tapped Fired", "Image_Tapped Fired", "ok");
+        }
+        
+
+    }
+}
